b3d8328c6ecc9052c6d0eb73d04aca31b6971eae
Emilio Valladares <vallae.wumbo@gmail.com>
2019-08-24 21:02:24 -0700

    On click function contents moved to start function and replace with a call for the start function.

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,14 +1,7 @@
 // Starts Quiz
 $("#start").on("click", function(){
-    // Removes start button
-    $("#start").remove();
-    // Appends questions and choices
-    for (var i = 0; i < questions.length; i++){
-        $("#subwrapper").append('<h3>' + questions[i].question + '</h3>');
-        for (var j = 0; j < questions[i].answers.length; j++){
-            $("#subwrapper").append("<input type = 'radio' name = 'question-" + i + "'value = '" + questions[i].answers[j] + "'>" + questions[i].answers[j])
-        }
-    }
+    // Calls start function to append question and choices
+    game.start();
 });
 
 var questions = [{
@@ -74,5 +67,17 @@ var game = {
             console.log("Time is up!");
             game.done();
         }
-    }
+    },
+
+    start: function(){
+        // Removes start button
+        $("#start").remove();
+        // Appends questions and choices to the page
+        for (var i = 0; i < questions.length; i++){
+            $("#subwrapper").append("<h3>" + questions[i].question + "</h3>");
+            for (var j = 0; j < questions[i].answers.length; j++){
+                $("#subwrapper").append("<input type = 'radio' name = 'question-" + i + "'value = '" + questions[i].answers[j] + "'>" + questions[i].answers[j])
+            }
+        }
+    },
 }
